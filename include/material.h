72335f89ce03efba0fd4e8d390ec094f79f9a1e0
ColeJMitchell <colejmitchell.cs@gmail.com>
2025-08-30 12:54:50 -0400

    setup for material system

---
+++ include/material.h
@@ -0,0 +1,18 @@
+#include 
+
+class Material
+{
+    public:
+        enum MaterialType
+        {
+            STEEL,
+            ALUMINIUM,
+            COPPER,
+            PLA,
+            PTEG
+        };
+
+        int getMaterialColor();
+
+
+};
